0fb32497ec29c6f480b5595e5fdb8e79f9228669
Vascon1 <166781378+Vascon11@users.noreply.github.com>
2024-08-26 01:27:35 -0300

    Taboada.py

--- Taboada.py
+++ Taboada.py
@@ -1,8 +1,17 @@
-while True:
-    x=int(input("Qual taboada quer saber?[numero negativo cancela] "))
-    if x <0:
-        break
-    print("-"*15)
-    for y in range(1,11):
-        print(f"{x} X {y} = {x*y}")
-    print("-"*15)
+while True:  #while loop infinito que continua até encontrar um break
+    # solicita ao usuário que insira o número da tabuada que ele deseja
+    x = int(input("Qual tabuada quer saber? [número negativo cancela] ")) 
+    
+    if x < 0:  #verifica se o número é negativo
+        break  #se for negativo, encerra o loop e termina o programa
+    
+    #imprime uma linha de separação para formatar a saída
+    print("-" * 15)  
+    
+    #for loop para calcular e imprimir a tabuada do número inserido
+    for y in range(1, 11):  
+        # imprime o resultado da multiplicação na forma 'x X y = resultado'
+        print(f"{x} X {y} = {x*y}")  
+    
+    #imprime outra linha de separação para finalizar a tabuada
+    print("-" * 15)

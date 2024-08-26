40beb6dd948f17cbb87b068e349917d78929071d
Vascon1 <166781378+Vascon11@users.noreply.github.com>
2024-08-26 01:28:51 -0300

    Update jogo-da-advinhaçao.py

--- jogo-da-advinhaçao.py
+++ jogo-da-advinhaçao.py
@@ -1,15 +1,27 @@
-import random
-pal=0
+import random  # importa o módulo random para gerar números aleatórios
 
+pal = 0  # inicializa o contador de palpites com 0
+
+# gera um número aleatório entre 0 e 10 e o armazena na variável y
 y = random.randint(0, 10)
-print("Acabei de pensar em um numero entre 0 e 10!")
-x=int(input("Voce consegue adivinhar o numero? "))
-while x !=y:
-    if x>y:
-        x=int(input("O numero que eu pensei é menor!"))
-        pal+=1
-    else:
-        x=int(input("O numero que eu pensei é maior: "))
-        pal+=1
-    print("-=-=-="*3,"-")
-print("Parabéns, voce acertou! depois de {} tentativas".format(pal))
+
+# informa ao usuário que o programa pensou em um número entre 0 e 10
+print("Acabei de pensar em um número entre 0 e 10!")
+
+# solicita ao usuário que tente adivinhar o número
+x = int(input("Você consegue adivinhar o número? "))
+
+# while loop que continua até que o usuário acerte o número
+while x != y:
+    if x > y:  # se o palpite do usuário for maior que o número, informa que o número é menor
+        x = int(input("O número que eu pensei é menor! "))
+        pal += 1  # incrementa o contador de palpites
+    else:  # se o palpite do usuário for menor que o número, informa que o número é maior
+        x = int(input("O número que eu pensei é maior! "))
+        pal += 1  # incrementa o contador de palpites
+    
+    # imprime uma linha de separação para formatar a saída
+    print("-=-=-=" * 3, "-")
+    
+# quando o usuário acertar, imprime a mensagem de parabéns e o número de tentativas
+print("Parabéns, você acertou! Depois de {} tentativas".format(pal))

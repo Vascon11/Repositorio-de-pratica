eba471480f380475d3d447217d9d845876a6389a
Vascon1 <166781378+Vascon11@users.noreply.github.com>
2024-08-26 01:29:25 -0300

    documento.py

--- documento.py
+++ documento.py
@@ -1,4 +1,10 @@
+# solicita ao usuário que digite seu sexo e converte a entrada para maiúsculas, removendo espaços extras
 sex = str(input('Digite seu sexo: ')).upper().strip()
-while sex not in "MmFf":
+
+# while loop que continua enquanto a entrada não for 'M' ou 'F'
+while sex not in "MF":
+    # solicita novamente o sexo caso a entrada seja inválida
     sex = str(input("Digite o seu sexo [M/F]: ")).upper().strip()
-print("Sua sexualidade foi registrada com secesso ,{}".format(sex))
+
+# exibe uma mensagem de confirmação quando o sexo for registrado corretamente
+print("Seu sexo foi registrado com sucesso: {}".format(sex))

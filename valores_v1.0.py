3213233b9ecd556705d9ca08263024edae6b8e01
Vascon1 <166781378+Vascon11@users.noreply.github.com>
2024-08-26 01:28:18 -0300

    Update valores_v1.0.py

--- valores_v1.0.py
+++ valores_v1.0.py
@@ -1,7 +1,13 @@
-h=z=0
-x=int(input("Digite um numero para somar [digite 999 para parar]: "))
-while x!=999:
-    h+=x
-    z+=1
-    x=int(input("Digite um numero para somar [digite 999 para parar]: "))
-print("voce digitou {} numeros com a soma resuldando em {}".format(z,h))
+h = z = 0  # inicializa as variáveis h (soma dos números) e z (contador de números) com 0
+
+# solicita ao usuário que insira um número para somar, informando que 999 encerra o programa
+x = int(input("Digite um número para somar [digite 999 para parar]: "))
+
+while x != 999:  # enquanto o número inserido for diferente de 999, o loop continua
+    h += x  # adiciona o número inserido à soma total
+    z += 1  # incrementa o contador de números digitados
+    # solicita ao usuário que insira outro número para somar
+    x = int(input("Digite um número para somar [digite 999 para parar]: "))
+
+# após sair do loop, imprime a quantidade de números digitados e a soma total
+print("Você digitou {} números com a soma resultando em {}".format(z, h))

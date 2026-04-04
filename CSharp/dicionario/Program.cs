24564e81eef55f94e10dc239518a9f3edb5e1b59
Vascon1 <166781378+Vascon11@users.noreply.github.com>
2026-04-04 15:57:07 -0300

    Add files via upload

---
+++ CSharp/dicionario/Program.cs
@@ -0,0 +1,20 @@
+﻿//Criar um dicionário que represente um aluno, com uma lista de notas
+//e mostre a média de suas notas na tela.
+
+Dictionary<string, int> alunos = new Dictionary<string, int>
+{
+    ["Alice"] = 9,
+    ["Roberto"] = 5,
+    ["Maria"] = 8,
+    ["Gabriel"] = 10,
+    ["Joao"] = 2
+};
+int quantidade=0;
+int soma = 0;
+foreach(KeyValuePair<String, int> notas in alunos)
+{
+    Console.WriteLine($"Nome: {notas.Key} - nota: {notas.Value}");
+    quantidade++;
+    soma+=notas.Value;
+}
+Console.WriteLine($"A media da turma foi de {(double)soma/quantidade}");

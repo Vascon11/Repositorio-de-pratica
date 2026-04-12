e24efe2677ce7deed6670b67ff85b902e3a0e65f
Vascon1 <166781378+Vascon11@users.noreply.github.com>
2026-04-12 16:21:54 -0300

    Add files via upload

---
+++ CSharp/cadastro/Program.cs
@@ -0,0 +1,36 @@
+﻿//Criar um programa que simule um sistema de login utilizando um dicionário para armazenar nomes de usuário e senhas.
+
+Dictionary<String, String> Cadastros = new Dictionary<string, string>
+{
+    ["vascon1"]="senha123"
+};
+
+void Validar(string usuarioDigitado, string senhaDigitada)
+{
+    while (true)
+    {
+        if (Cadastros.ContainsKey(usuarioDigitado) && Cadastros[usuarioDigitado]==senhaDigitada)
+    {
+        Console.WriteLine($"\nAcesso liberado! Bem-vindo, {usuarioDigitado}.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Usuário ou senha incorretos.");
+    }
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Qual é seu usuario?");
+String user = Console.ReadLine()!;
+if (Cadastros.ContainsKey(user))
+{
+    Console.WriteLine("\nDigite sua senha:");
+    String password = Console.ReadLine()!;
+    Validar(user, password);
+}
+else
+{
+    Console.WriteLine($"Não foi encontrado o usuario com o nome {user}!");
+}
